a04ddbaf8ee79f42a5ac5249ade6b5d5e1952490
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-02 10:44:30 +0100

    Adds typeAdapter to awt.Color. Physicks world can be saved to json

--- src/main/java/game/WindowBasket.java
+++ src/main/java/game/WindowBasket.java
@@ -3,8 +3,6 @@ package game;
 import game.engine.Simulation;
 import game.engine.Vector2D;
 
-import com.google.gson.*;
-
 public class WindowBasket {
 
     Simulation sim = new Simulation(new Vector2D(500, 300));
