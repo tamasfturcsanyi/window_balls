0c0a18e5b3bf4b6258d77dad44c7709643db2795
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-25 17:25:10 +0100

    adds comment to SimulationButton

--- src/main/java/game/controller/SimulationButton.java
+++ src/main/java/game/controller/SimulationButton.java
@@ -10,12 +10,24 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
 
+/**
+ * The SimulationButton class extends JButton and represents a button that is used to 
+ * start a simulation based on a JSON file. The button displays the title of the simulation 
+ * and an icon generated from the simulation visuals.
+ */
 public class SimulationButton extends JButton{
 
     boolean selected = false;
 
     String jsonPath;
 
+    /**
+     * Constructs a SimulationButton with the specified JSON file.
+     * Initializes the button with the title from the simulation window,
+     * sets the font, icon, and text positions.
+     * 
+     * @param jsonFile the JSON file containing simulation data
+     */
     public SimulationButton(File jsonFile){
         jsonPath = jsonFile.getAbsolutePath();
         SimulationWindow simWindow = new SimulationWindow(jsonPath);
@@ -36,6 +48,11 @@ public class SimulationButton extends JButton{
         this.setVerticalTextPosition(SwingConstants.TOP);   // Text above the image
     }
 
+    /**
+     * Updates the appearance of the button based on its selection state.
+     * If the button is not selected, it sets the background color to a light blue.
+     * If the button is selected, it sets the background color to yellow.
+     */
     public void updateApperence() {
         if(!selected){
             setBackground(Color.decode("#63d8ff"));
