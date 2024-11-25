70b068a68d4a062aa7b8e136872f8af494f8ee85
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-25 17:07:57 +0100

    Renames Main class to WindowBalls

--- src/main/java/game/WindowBalls.java
+++ src/main/java/game/WindowBalls.java
@@ -2,9 +2,14 @@ package game;
 
 import game.controller.MainMenu;
 
-public class WindowBasket {
+public class WindowBalls {
     void run(){
         Thread simulationThread = new Thread(new MainMenu());
         simulationThread.start();
+    }
+
+    public static void main(String[] args) {
+        WindowBalls basket = new WindowBalls();
+        basket.run();
     }
 }
