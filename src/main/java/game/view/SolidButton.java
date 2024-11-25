48839860df5e621a03dfe7916e63822a207e31b0
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-25 18:50:25 +0100

    adds comment to SolidButton

--- src/main/java/game/view/SolidButton.java
+++ src/main/java/game/view/SolidButton.java
@@ -11,11 +11,23 @@ import game.model.physicsbodies.Brick;
 
 import java.awt.Rectangle;
 
+/**
+ * SolidButton is a custom JButton with additional features such as a Brick body,
+ * hover effect, and custom styling.
+ * It participates in the Simulation while being functional as a JButton.
+ */
 public class SolidButton extends JButton{
     Color normalColor = Color.WHITE;
     
     transient Brick body;
 
+    /**
+     * Constructs a SolidButton with the specified title, position, and dimensions.
+     *
+     * @param title the title of the button
+     * @param position the position of the button as a Vector2D object
+     * @param dimensions the dimensions of the button as a Vector2D object
+     */
     public SolidButton(String title, Vector2D position, Vector2D dimensions){
         super(title);
         body = new Brick(position, dimensions);
@@ -26,6 +38,12 @@ public class SolidButton extends JButton{
         return body;
     }
 
+    /**
+     * Updates the appearance of the button based on its rollover state.
+     * If the button is in a rollover state (i.e., the mouse is hovering over it),
+     * the background color is set to a darker shade of the normal color.
+     * Otherwise, the background color is set to the normal color.
+     */
     public void updateApperence(){
         ButtonModel model = getModel();
         if (model.isRollover()) {
@@ -36,6 +54,11 @@ public class SolidButton extends JButton{
         }
     }
 
+    /**
+     * Updates the position of the button based on the given window bounds, so it appears fixed
+     *
+     * @param windowBounds the bounds of the window used to adjust the button's position
+     */
     public void updatePosition(Rectangle windowBounds){
         this.setBounds( (int)body.getPosition().getX() - windowBounds.x,
                         (int)body.getPosition().getY() - windowBounds.y,
@@ -43,6 +66,12 @@ public class SolidButton extends JButton{
                         (int)body.getDimension().getY());
     }
 
+    /**
+     * Applies the "Impact" style to the button.
+     * This includes setting the font to "Impact" with bold style and size 32,
+     * painting the border with a black line of thickness 5,
+     * and disabling the focus painting.
+     */
     public void impactStyle(){
         setFont(new Font("Impact",Font.BOLD,32));
         setBorderPainted(true);
