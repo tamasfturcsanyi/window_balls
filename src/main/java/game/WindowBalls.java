8e297fb2a0529bf1cb980a58903caba34d63b93b
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-25 18:59:53 +0100

    adds Comment

--- src/main/java/game/WindowBalls.java
+++ src/main/java/game/WindowBalls.java
@@ -1,13 +1,22 @@
 package game;
 
 import game.controller.MainMenu;
-
+/**
+ * Main class of the application. It starts the main menu.
+ */
 public class WindowBalls {
+    /**
+     * Method that starts the main menu.
+     */
     void run(){
         Thread simulationThread = new Thread(new MainMenu());
         simulationThread.start();
     }
 
+    /**
+     * Main method that starts the application.
+     * @param args
+     */
     public static void main(String[] args) {
         WindowBalls basket = new WindowBalls();
         basket.run();
