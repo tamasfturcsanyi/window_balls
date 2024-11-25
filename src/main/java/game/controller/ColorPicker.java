4bdd7c7027213a7cea3eb3391c61653996d2cee6
Turcsányi Tamás <tamas.f.turcsanyi@gmail.com>
2024-11-25 16:36:03 +0100

    Adds Comment to ColorPicker

--- src/main/java/game/controller/ColorPicker.java
+++ src/main/java/game/controller/ColorPicker.java
@@ -4,15 +4,45 @@ import java.awt.Color;
 
 import javax.swing.JComboBox;
 
+/**
+ * The ColorPicker class extends JComboBox<String> to provide a dropdown menu
+ * for selecting colors. It initializes with a predefined list of color names.
+ * 
+ * The class provides methods to get the selected color as a Color object.
+ * 
+ * Methods:
+ * - ColorPicker(): Constructor that initializes the JComboBox with color names.
+ * - Color getColor(): Returns the selected color as a Color object.
+ * - Color toColor(String colorName): Converts a color name to a corresponding Color object.
+ * 
+ * Example usage:
+ * ColorPicker colorPicker = new ColorPicker();
+ * Color selectedColor = colorPicker.getColor();
+ */
 public class ColorPicker extends JComboBox<String> {
+    /**
+     * Constructs a new ColorPicker with a predefined set of color options.
+     * The available colors are: Red, Green, Blue, Yellow, Black, White, Gray, Cyan, Magenta, Pink, and Orange.
+     */
     public ColorPicker(){
         super(new String[]{"Red", "Green", "Blue", "Yellow", "Black", "White", "Gray", "Cyan", "Magenta", "Pink", "Orange"});
     }
 
+    /**
+     * Retrieves the currently selected color.
+     *
+     * @return the selected color as a {@link Color} object
+     */
     Color getColor(){
         return toColor((String)getSelectedItem());
     }
     
+    /**
+     * Converts a color name to its corresponding {@link Color} object.
+     *
+     * @param colorName the name of the color as a {@link String}
+     * @return the corresponding {@link Color} object, or {@link Color#BLACK} if the color name is not recognized
+     */
     Color toColor(String colorName){
         switch (colorName) {
             case "Red":
